4abf77a52e31638730af4a099413634a013026b3
Iova Nicoleta Carmen <148574222+nicoletaiova25@users.noreply.github.com>
2023-10-24 23:56:03 +0300

    Update sketch_oct23b_copy_20231024232706.ino

--- Homework/sketch_oct23b_copy_20231024232706.ino
+++ Homework/sketch_oct23b_copy_20231024232706.ino
@@ -1,20 +1,26 @@
 ///declaram variabilele
+///declaram pinii Analog
 const int pinAnalogRosu=A0;
 const int pinAnalogVerde=A1;
 const int pinAnalogBlue=A2;
 
+///declaram pinii digitali; am facut corespondenta intre culorile ledului si numarul pinului 
+//dupa conventia pinilor ledului RGB, iar apoi am testat prin trial and error
 const int ledPinRosu=11;
 const int ledPinVerde=10;
 const int ledPinBlue=9;
 
+//declaram variabilele in care vom citi valoarea fiecarui potentiometru
 float potValueRosu = 0;
 float potValueVerde = 0;
 float potValueBlue = 0;
 
+//declaram variabilele in care vom retine valoarea mapata pentru afisare
 float ledValueRosu = 0;
 float ledValueVerde = 0;
 float ledValueBlue = 0;
 
+//declaram variabile pentru a evita folosirea numerelor magice 
 int inputAnalogMin = 0;
 int inputAnalogMax= 1023;
 int ledPinMin=0;
@@ -27,6 +33,7 @@ void setup() {
   pinMode(ledPinBlue, OUTPUT);
 }
 
+//functie de citire a valorilor potentiometrelor inregistrate prin pinii analogi
 void citireAnalog()
 {
   potValueRosu = analogRead(pinAnalogRosu);
@@ -34,6 +41,7 @@ void citireAnalog()
   potValueBlue = analogRead(pinAnalogBlue);
 }
 
+//functie in care mapam in functie de valoarea citita pe potentiometru si valorile declarate anterior
 void mapare()
 {
   ledValueRosu = map(potValueRosu, inputAnalogMin, inputAnalogMax, ledPinMin, ledPinMax);
@@ -41,6 +49,7 @@ void mapare()
   ledValueBlue = map(potValueBlue, inputAnalogMin, inputAnalogMax, ledPinMin, ledPinMax);
 }
 
+//functie de scriere pentru a afisa culorile ledului
 void ScrieAnalog()
 {
   analogWrite(ledPinRosu, ledValueRosu);
@@ -50,6 +59,7 @@ void ScrieAnalog()
 
 void loop() {
   // put your main code here, to run repeatedly:
+  //apelarea functiilor
   citireAnalog();
   mapare();
   ScrieAnalog();
